fe72baf40ddcc880a98eeeb33cf86720a808e75a
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-15 10:11:35 -0700

    created letter and input for name

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#formOne").submit(function(event) {
+      var person1Input = $("input#name").val();
+
+      $(".name").text(person1Input);
+      $("#letter").show();
+  
+      event.preventDefault();
+    });
+  });
